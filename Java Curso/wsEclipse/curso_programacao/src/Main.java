f801b1d91ee46ad53775050cc2514529d78e8a5d
ecacarva <ecacarva@uem.br>
2023-05-29 14:34:25 -0300

    Update

--- Java Curso/wsEclipse/curso_programacao/src/Main.java
+++ Java Curso/wsEclipse/curso_programacao/src/Main.java
@@ -284,9 +284,9 @@ public class Main {
 		}
 		System.out.println(soma);
 		sc.close();
-		 
-*/
-		Locale.setDefault(Locale.US);
+		
+	// while
+	 	Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		char resp = 's';
@@ -297,10 +297,30 @@ public class Main {
 			double C = sc.nextDouble();
 			double F = 9.0 * C / 5.0 + 32.0;
 			
-			System.out.printf("Equivalente em Fahrenheit: %.2fn",F);
+			System.out.printf("Equivalente em Fahrenheit: %.2f%n",F);
 			System.out.print("\nDeseja repetir (s/n)?");
 			resp = sc.next().charAt(0);	
 	    }
+		
+		sc.close();
+		
+	}
+		
+		 
+*/
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+				
+	    do  {
+	    	System.out.println("Digite a temperatura em Celsius:");
+			
+			double C = sc.nextDouble();
+			double F = 9.0 * C / 5.0 + 32.0;
+			
+			System.out.printf("Equivalente em Fahrenheit: %.2f%n",F);
+			System.out.print("\nDeseja repetir (s/n)?");
+			char resp = sc.next().charAt(0);	
+	    } while (resp !='n');
 		
 		sc.close();
 		
